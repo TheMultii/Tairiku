1c4382dffdc5e54d216da80b0e69dfc29f20d143
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-09-30 19:19:25 +0200

    Fixed masonry issues that caused images to not be deleted from the site when changing categories.

--- src/routes/TairikuCategory.tsx
+++ src/routes/TairikuCategory.tsx
@@ -6,11 +6,13 @@ import {
   usePositioner,
   useResizeObserver,
   useScroller,
+  type LoadMoreItemsCallback,
+  type Positioner,
 } from 'masonic'
 import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { useTitle, useUnmount, useWindowSize } from 'react-use'
-import { Footer, TairikuMasonryItem } from '../components'
+import { TairikuMasonryItem } from '../components'
 import { useTairikuCategories } from '../providers'
 import {
   type TairikuCategoryDescription,
@@ -39,6 +41,7 @@ export const TairikuCategory = () => {
   const [tairikuCategoryImages, setTairikuCategoryImages] = useState<
     TairikuGeneratedImage[]
   >([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const [containerWidth, setContainerWidth] = useState(0)
   const [masonryStartIndex, setMasonryStartIndex] = useState(-1)
@@ -94,57 +97,117 @@ export const TairikuCategory = () => {
 
   const fetchImages = async () => {
     const abortController = new AbortController()
-    const { data } = await axios.get<TairikuGeneratedImageResponse>(
-      `https://api.mganczarczyk.pl/tairiku/${categoryData.name}?count=25`,
-      {
-        signal: abortController.signal,
-      }
-    )
 
-    setTairikuCategoryImages((prev) => [...prev, ...data.images])
+    try {
+      setIsLoading(true)
+
+      const { data } = await axios.get<TairikuGeneratedImageResponse>(
+        `https://api.mganczarczyk.pl/tairiku/${categoryData.name}?count=25`,
+        {
+          signal: abortController.signal,
+        }
+      )
+
+      setTairikuCategoryImages((prev) => [...prev, ...data.images])
+    } finally {
+      setIsLoading(false)
+    }
 
     return () => {
       abortController.abort()
     }
   }
+
   useEffect(() => {
+    setTairikuCategoryImages([])
     fetchImages()
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [categoryData.name])
 
   useUnmount(() => {
     document.title = 'Tairiku'
   })
+
+  const removeFromArrayCallback = (id: number | string) => {
+    setTairikuCategoryImages((prev) => prev.filter((image) => image.id !== id))
+  }
 
   return (
     <div className="w-full">
       <div className="relative mb-2 h-[40vh] after:absolute after:bottom-0 after:left-0 after:h-full after:w-full after:bg-gradient-to-b after:from-transparent after:via-transparent after:via-60% after:to-dark-100 after:to-[99%]">
         <img
           className="h-[99%] w-full object-cover"
-          src="https://beta.mganczarczyk.pl/assets/ubb_timetable_card.webp"
+          src="https://mganczarczyk.pl/assets/ubb_timetable_card.webp"
           alt=""
         />
-        <div className="relative z-10 -mt-16 text-center">
+        <div className="relative z-[5] -mt-16 text-center">
           <Link to="/">
             <p className="text-4xl font-bold">{categoryData.pretty_name}</p>
           </Link>
         </div>
       </div>
-      <main className="mx-1 mb-8 mt-8 sm:mx-2 sm:mt-10 md:mx-4" ref={mainref}>
-        {useMasonry({
-          scrollTop,
-          positioner,
-          height,
-          containerRef,
-          resizeObserver,
-          overscanBy: 1.5,
-          items: tairikuCategoryImages,
-          onRender: checkShouldFetchNextBatch,
-          render: TairikuMasonryItem,
-        })}
+      <main
+        className="mx-1 mb-8 mt-8 flex flex-col items-center justify-center sm:mx-2 sm:mt-10 md:mx-4"
+        ref={mainref}
+      >
+        {tairikuCategoryImages.length > 0 && (
+          <TairikuMasonry
+            scrollTop={scrollTop}
+            positioner={positioner}
+            height={height}
+            containerRef={containerRef}
+            resizeObserver={resizeObserver}
+            tairikuCategoryImages={tairikuCategoryImages}
+            removeFromArrayCallback={removeFromArrayCallback}
+            checkShouldFetchNextBatch={checkShouldFetchNextBatch}
+          />
+        )}
+        {isLoading && (
+          <div className="spin h-8 w-8 rounded-full border-4 border-dark-300 border-t-primary" />
+        )}
       </main>
-      <Footer />
     </div>
+  )
+}
+
+interface TairikuMasonryProps {
+  scrollTop: number
+  positioner: Positioner
+  height: number
+  containerRef: React.RefObject<HTMLElement>
+  resizeObserver: ResizeObserver
+  tairikuCategoryImages: TairikuGeneratedImage[]
+  removeFromArrayCallback: (id: number | string) => void
+  checkShouldFetchNextBatch: LoadMoreItemsCallback<unknown>
+}
+
+const TairikuMasonry = ({
+  scrollTop,
+  positioner,
+  height,
+  containerRef,
+  resizeObserver,
+  tairikuCategoryImages,
+  removeFromArrayCallback,
+  checkShouldFetchNextBatch,
+}: TairikuMasonryProps) => {
+  return (
+    <>
+      {useMasonry({
+        scrollTop,
+        positioner,
+        height,
+        containerRef,
+        resizeObserver,
+        overscanBy: 1.5,
+        items: tairikuCategoryImages.map((image) => ({
+          image,
+          removeFromArrayCallback,
+        })),
+        onRender: checkShouldFetchNextBatch,
+        render: TairikuMasonryItem,
+      })}
+    </>
   )
 }
