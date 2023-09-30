710402345327b19e65956230bb701852c642fe5d
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2023-09-30 19:20:01 +0200

    Added a profile banner/avatar image to the test page

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -17,20 +17,27 @@ export const Test = () => {
   }, [])
 
   return (
-    <Link to="/">
-      <h1>
-        Logged in as{' '}
+    <div className="mx-4 mb-2 mt-[55px] overflow-hidden">
+      <img
+        className="pointer-events-none relative -z-10 mx-auto h-[200px] w-full select-none rounded-md object-cover lg:w-3/4"
+        src={`https://api.mganczarczyk.pl/user/${
+          user?.profile.preferred_username ?? ''
+        }/header`}
+        alt={`${user?.profile.preferred_username ?? ''} profile banner`}
+      />
+      <img
+        className="pointer-events-none mx-auto mt-[-62.5px] h-[125px] w-[125px] select-none rounded-full border-8 border-solid border-dark-100 object-cover sm:mt-[-75px] sm:h-[150px] sm:w-[150px]"
+        src={`https://api.mganczarczyk.pl/user/${
+          user?.profile.preferred_username ?? ''
+        }/profile`}
+        alt={`${user?.profile.preferred_username ?? ''} profile avatar`}
+      />
+      <h1 className="mb-10 text-center text-3xl sm:text-4xl">
         {user != null ? user?.profile.preferred_username : 'nobody'}
       </h1>
-      <h2>Token: {user != null ? user?.access_token : ''}</h2>
-      <h3>
-        Permissions:{' '}
-        {user != null
-          ? user?.realm_access.roles.map((e, _) => {
-              return `${e}, `
-            })
-          : '/'}
-      </h3>
-    </Link>
+      <Link to="/" className="block text-center uppercase text-primary">
+        go back home
+      </Link>
+    </div>
   )
 }
